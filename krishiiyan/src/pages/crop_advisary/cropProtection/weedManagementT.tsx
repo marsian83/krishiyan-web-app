32ad5c5c30981810c651c9c34733c4c79ceb7ca8
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-08-15 21:17:31 +0530

    Add weed management in frontend

--- krishiiyan/src/pages/crop_advisary/cropProtection/weedManagementT.tsx
+++ krishiiyan/src/pages/crop_advisary/cropProtection/weedManagementT.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { extractCodeFromDriveLink } from "../../../handleImageCode";
 
 const WeedManagementT = (props: any) => {
-  const [table, setTable] = useState(props.data.WeedMangement.value);
+  const [table, setTable] = useState(props.data.weedManagement);
+  console.log(table)
   return (
     <>
       <table className="table-auto border-collapse border border-black font-bold text-base w-[80%] mx-auto mt-10">
@@ -20,196 +22,42 @@ const WeedManagementT = (props: any) => {
             <th className="border-r border-black py-[1.2%]">
               Solution (Product)
             </th>
-            <th className="border-r border-black py-[1.2%]">
-              Reccomendation description
-            </th>
           </tr>
         </thead>
         <tbody>
-          {/* Stage1 */}
-          {/* {oldCultivation?.map((cultivation: any, index: any) => ( */}
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">1</td>
-            <td className="border-r border-black font-thin">
-              {table.value1.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value1.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value1.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value1.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value1.solution}
-            </td>
-          </tr>
+        {
+            table.map((item: any, index: any) => {
+              return (
+                <tr className="h-10 border-b border-black">
+                <td className="border-r border-black font-thin">{index+1}</td>
+                <td className="border-r border-black font-thin">
+                  {
+                    item.category
+                  }
+                </td>
+                <td className="border-r border-black font-thin">
+                  {
+                    item.name
 
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">2</td>
-            <td className="border-r border-black font-thin">
-              {table.value2.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value2.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value2.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value2.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value2.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">3</td>
-            <td className="border-r border-black font-thin">
-              {table.value3.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value3.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value3.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value3.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value3.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">4</td>
-            <td className="border-r border-black font-thin">
-              {table.value4.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value4.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value4.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value4.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value4.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">5</td>
-            <td className="border-r border-black font-thin">
-              {table.value5.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value5.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value5.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value5.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value5.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">6</td>
-            <td className="border-r border-black font-thin">
-              {table.value6.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value6.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value6.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value6.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value6.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">7</td>
-            <td className="border-r border-black font-thin">
-              {table.value7.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value7.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value7.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value7.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value7.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">8</td>
-            <td className="border-r border-black font-thin">
-              {table.value8.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value8.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value8.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value8.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value8.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">9</td>
-            <td className="border-r border-black font-thin">
-              {table.value9.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value9.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value9.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value9.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value9.solution}
-            </td>
-          </tr>
-          <tr className="h-10 border-b border-black">
-            <td className="border-r border-black font-thin">10</td>
-            <td className="border-r border-black font-thin">
-              {table.value10.type_weed}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value10.weed_name}
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value10.scientificName}
-            </td>
-            <td className="border-r border-black font-thin">
-              <img src={table.value10.image} />
-            </td>
-            <td className="border-r border-black font-thin">
-              {table.value10.solution}
-            </td>
-          </tr>
-          {/* ))} */}
+                  }
+                </td>
+                <td className="border-r border-black font-thin">
+                  {
+                    item.scientificName
+                  }
+                </td>
+                <td className="border-r border-black font-thin">
+                        <img src={`https://drive.google.com/uc?export=view&id=${extractCodeFromDriveLink(item.image)}`} style={{ marginTop: "20px" ,width:"200px", height:"200px", objectFit:"cover" }} />
+                </td>
+                <td className="border-r border-black font-thin">
+                  {
+                    item.solutions
+                  }
+                </td>
+              </tr>
+              )
+            })
+          }
         </tbody>
       </table>
     </>
