e428734b978b58cc08c913f4ce147c6729a9d426
Harcharan Singh <bcs_2021028@iiitm.ac.in>
2024-07-01 09:15:04 -0700

    minimal

--- models/DataModel.js
+++ models/DataModel.js
@@ -3,15 +3,12 @@ const mongoose = require("mongoose");
 const participantSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
   },
   contact: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
   },
 });
 
